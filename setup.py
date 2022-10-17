431cd788ae962b916795cdb806934b92918706ab
nickhir <nick.hirschmueller@yahoo.de>
2022-10-17 19:04:47 +0200

    added progress bar

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from pathlib import Path
 
-VERSION = '1.0.0'
+VERSION = '1.0.1'
 DESCRIPTION = 'Alternative for unix mv, which searches through your files and updates any references to the file that you just moved.'
 this_directory = Path(__file__).parent
 LONG_DESCRIPTION = (this_directory / "README.md").read_text()
@@ -14,6 +14,7 @@ setup(
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
+    url="https://github.com/nickhir/pyfilemv/",
     packages=find_packages(),
     install_requires=['colorama'],
     keywords=['python', 'filesystem', 'move', 'renaming', 'organization'],
